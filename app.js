5ca859f55e28f982c9ec3523a6388be3ee8d7c1e
amirrezaqqf-lgtm <amirrezaqqf@gmail.com>
2026-01-06 11:51:57 +0330

    Update app.js

--- app.js
+++ app.js
@@ -47,6 +47,11 @@ function createTodoItem(todo, todoIndex){
         <label for="${todoId}" class="todo-text">
             ${todoText}
         </label>
+        <button class="edit-button">
+            <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
+                <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
+            </svg>
+        </button>
         <button class="delete-button">
             <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
                 <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
@@ -59,29 +64,18 @@ function createTodoItem(todo, todoIndex){
         deleteTodoItem(todoIndex);
     });
     
+    const editBtn = todoLI.querySelector('.edit-button');
+    editBtn.addEventListener('click', function() {
+        editTodoItem(todoIndex);
+    });
+    
     const checkbox = todoLI.querySelector('input[type="checkbox"]');
-    const todoTextLabel = todoLI.querySelector('.todo-text');
     
     checkbox.checked = todo.completed;
-    if(todo.completed){
-        todoTextLabel.style.textDecoration = "line-through";
-        todoTextLabel.style.opacity = "0.7";
-    } else {
-        todoTextLabel.style.textDecoration = "none";
-        todoTextLabel.style.opacity = "1";
-    }
     
     checkbox.addEventListener('change', function() {
         allTodos[todoIndex].completed = this.checked;
         savetodos();
-        
-        if(this.checked){
-            todoTextLabel.style.textDecoration = "line-through";
-            todoTextLabel.style.opacity = "0.7";
-        } else {
-            todoTextLabel.style.textDecoration = "none";
-            todoTextLabel.style.opacity = "1";
-        }
     });
     
     return todoLI;
@@ -91,6 +85,15 @@ function deleteTodoItem(todoIndex){
     allTodos = allTodos.filter((_, i) => i !== todoIndex);
     savetodos();
     updateTodoList();
+}
+
+function editTodoItem(todoIndex){
+    const newText = prompt("ویرایش تسک:", allTodos[todoIndex].text);
+    if(newText !== null && newText.trim() !== ""){
+        allTodos[todoIndex].text = newText.trim();
+        savetodos();
+        updateTodoList();
+    }
 }
 
 function savetodos(){
